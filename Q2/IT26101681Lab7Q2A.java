82611e55b402644414cf66b1a0f1a2dd0a9832e7
IT26101681 <IT26101681@my.sliit.lk>
2026-09-13 22:57:49 +0530

    Add files via upload

---
+++ Q2/IT26101681Lab7Q2A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT26101681Lab7Q2A {
+
+    public static void main(String[] args) {
+
+        
+        int rows = 4;
+        int columns = 5;
+
+        
+        for (int rowCount = 0; rowCount < rows; rowCount++) {
+
+            
+            for (int columnCount = 0; columnCount < columns; columnCount++) {
+                System.out.print("$ ");
+            }
+            
+
+            System.out.println();
+        }
+    }
+}
